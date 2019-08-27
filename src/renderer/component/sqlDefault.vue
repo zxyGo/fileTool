81951cfe87721bb131de36494feff366535f2888
CaoPengFlying <18679229291@163.com>
2019-08-27 21:12:14 +0800

    增加默认后端地址访问赋值

--- src/renderer/component/sqlDefault.vue
+++ src/renderer/component/sqlDefault.vue
@@ -116,6 +116,7 @@ export default {
     setDefaultValue() {
       const _tem = this.$store.state.Test.defaultConfig
       this.defaultItem = {
+        url: _tem.url,
         host: _tem.host,
         port: _tem.port ? _tem.port.toString() : '3306',
         database: '',
